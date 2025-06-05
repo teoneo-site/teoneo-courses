14e42daed8c52be66f67e0f181405156a4da7d19
klewy <cigankov807@gmail.com>
2025-06-05 21:56:15 +0300

    Less code yay

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -12,8 +12,7 @@ JSON:
 }
 */
 
-use axum::{extract::{Path, Query, State}, http::StatusCode, response::{IntoResponse, Response}};
-use axum_extra::extract::OptionalQuery;
+use axum::{extract::{Query, State}, http::StatusCode, response::{IntoResponse, Response}};
 use serde::{Deserialize, Serialize};
 
 use crate::{common::token::Claims, controllers, handlers::{self, ErrorTypes}, AppState};
@@ -37,66 +36,36 @@ pub async fn get_user_info_and_courses(
     Query(value) : Query<UserInfoQuery>,
 ) -> Result<Response, Response> {
     let user_id = claims.id;
-    let mut json_obj = serde_json::Value::Object(serde_json::Map::new());
-
-    match value.value {
-        ValueInfo::All => {
-            match controllers::user::get_user_info_all(&state, user_id).await {
-                Ok(info) => {
-                    json_obj["data"] = serde_json::to_value(&info).unwrap(); // Why would it panic?
-                    return Ok((StatusCode::OK, axum::Json(json_obj)).into_response())
-                },
-                Err(why) => {
-                    eprintln!("Why failed: {}", why);
-                    return Err((
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        axum::Json(handlers::ErrorResponse::new(
-                            ErrorTypes::InternalError,
-                            "Something happened",
-                        )), // Should not panic, because struct is always valid for converting into JSON
-                    )
-                        .into_response());
-                }
-            }   
-        }
-        ValueInfo::Courses => {
-            match controllers::user::get_courses_info(&state, user_id).await {
-                Ok(info) => {
-                    json_obj["data"] = serde_json::to_value(&info).unwrap(); // Why would it panic?
-                    return Ok((StatusCode::OK, axum::Json(json_obj)).into_response())
-                },
-                Err(why) => {
-                    eprintln!("Why failed: {}", why);
-                    return Err((
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        axum::Json(handlers::ErrorResponse::new(
-                            ErrorTypes::InternalError,
-                            "Something happened",
-                        )), // Should not panic, because struct is always valid for converting into JSON
-                    )
-                        .into_response());
-                }
+    async fn handle_result<T: Serialize>(fut: impl std::future::Future<Output = Result<T, anyhow::Error>>) -> Result<Response, Response> {
+        let mut json_obj = serde_json::Value::Object(serde_json::Map::new());
+        match fut.await {
+            Ok(info) => {
+                json_obj["data"] = serde_json::to_value(&info).unwrap(); // Why would it panic?
+                return Ok((StatusCode::OK, axum::Json(json_obj)).into_response())
             }
-        }
-        ValueInfo::User => {
-            match controllers::user::get_user_info(&state, user_id).await {
-                Ok(info) => {
-                    json_obj["data"] = serde_json::to_value(&info).unwrap(); // Why would it panic?
-                    return Ok((StatusCode::OK, axum::Json(json_obj)).into_response())
-                },
-                Err(why) => {
-                    eprintln!("Why failed: {}", why);
-                    return Err((
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        axum::Json(handlers::ErrorResponse::new(
-                            ErrorTypes::InternalError,
-                            "Something happened",
-                        )), // Should not panic, because struct is always valid for converting into JSON
-                    )
-                        .into_response());
-                }
+            Err(why) => {
+                eprintln!("Why failed: {}", why);
+                return Err((
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    axum::Json(handlers::ErrorResponse::new(
+                        ErrorTypes::InternalError,
+                        "Something happened",
+                    )), // Should not panic, because struct is always valid for converting into JSON
+                )
+                    .into_response());
             }
         }
     }
-   
+
+    match value.value {
+        ValueInfo::All => {
+            handle_result(controllers::user::get_user_info_all(&state, user_id)).await
+        }
+        ValueInfo::Courses => {
+            handle_result(controllers::user::get_courses_info(&state, user_id)).await
+        }
+        ValueInfo::User => {
+            handle_result(controllers::user::get_user_info(&state, user_id)).await
+        }
+    }
 }
