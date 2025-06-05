34aad1a2ef6e15e56454357535e7b3e4bf387a43
klewy <cigankov807@gmail.com>
2025-06-05 21:59:03 +0300

    Some beautifying

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -30,33 +30,33 @@ pub struct UserInfoQuery {
     value: ValueInfo
 }
 
+async fn handle_result<T: Serialize>(fut: impl std::future::Future<Output = Result<T, anyhow::Error>>) -> Result<Response, Response> {
+    let mut json_obj = serde_json::Value::Object(serde_json::Map::new());
+    match fut.await {
+        Ok(info) => {
+            json_obj["data"] = serde_json::to_value(&info).unwrap(); // Why would it panic?
+            return Ok((StatusCode::OK, axum::Json(json_obj)).into_response())
+        }
+        Err(why) => {
+            eprintln!("Why failed: {}", why);
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                axum::Json(handlers::ErrorResponse::new(
+                    ErrorTypes::InternalError,
+                    "Something happened",
+                )), // Should not panic, because struct is always valid for converting into JSON
+            )
+                .into_response());
+        }
+    }
+}
+
 pub async fn get_user_info_and_courses(
     State(state): State<AppState>,
     claims: Claims,
     Query(value) : Query<UserInfoQuery>,
 ) -> Result<Response, Response> {
     let user_id = claims.id;
-    async fn handle_result<T: Serialize>(fut: impl std::future::Future<Output = Result<T, anyhow::Error>>) -> Result<Response, Response> {
-        let mut json_obj = serde_json::Value::Object(serde_json::Map::new());
-        match fut.await {
-            Ok(info) => {
-                json_obj["data"] = serde_json::to_value(&info).unwrap(); // Why would it panic?
-                return Ok((StatusCode::OK, axum::Json(json_obj)).into_response())
-            }
-            Err(why) => {
-                eprintln!("Why failed: {}", why);
-                return Err((
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    axum::Json(handlers::ErrorResponse::new(
-                        ErrorTypes::InternalError,
-                        "Something happened",
-                    )), // Should not panic, because struct is always valid for converting into JSON
-                )
-                    .into_response());
-            }
-        }
-    }
-
     match value.value {
         ValueInfo::All => {
             handle_result(controllers::user::get_user_info_all(&state, user_id)).await
